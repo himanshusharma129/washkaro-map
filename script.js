31170561240e1a2f72540b120121ff80f29adf15
Himanshu Sharma <0hspandit0@gmail.com>
2020-04-03 15:38:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -112,13 +112,13 @@ function initMapStuff(){
     map.setView([22.5, 82], 4);
 
     if(is_touch_device()){
-        map.dragging.disable();
+       // map.dragging.disable();
         map.tap.disable();
     }
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVuYWlkYmFidSIsImEiOiJqc2ZuNkhFIn0.rdicmW4uRhYuHZxEK9dRbg', {
-    maxZoom: 6,
-    minZoom: 4,
+    maxZoom: 8,
+    minZoom: 6,
     id: 'mapbox/light-v9',
     tileSize: 512,
     zoomOffset: -1
